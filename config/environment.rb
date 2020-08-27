bb8b4048a75d8b1197903a101db2b888b7067279
johnnie71 <johnnie.agonz71@gmail.com>
2020-08-27 14:59:06 -0400

    Owner name and species with Christian A

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,19 @@
 Dir[File.join(File.dirname(__FILE__), "../lib", "*.rb")].each {|f| require f}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,37 @@
+require 'pry'
+
+
+
 class Owner
-  # code goes here
+
+  attr_reader :name, :dog, :species
+  attr_accessor :walk, :feed
+
+  @@all =[]
+
+  def initialize (name)
+    @name = name
+    @walk = walk
+    @feed = feed
+    @species = "human"
+    @@all << self
+  end
+
+  def name
+    @name
+  end
+
+  def say_species(species)
+    @species.name
+  end
+
+  def self.all
+    @all
+  end
+
+  def self.count
+
+  end 
+
+
 end
